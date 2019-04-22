f062acf5035c98d58dbcfe523a4035f63f9e1666
Ainour <AinourPk@163.com>
2019-04-22 15:18:07 +0800

    Use a function

--- src/main/java/MavenWithGit/App.java
+++ src/main/java/MavenWithGit/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello world!");
+        Use use = new Use();
+        int a = use.re(0);
+        System.out.println(a);
     }
 }
